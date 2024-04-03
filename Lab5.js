7028210ba4372f3318fdda481f27654d4ed750bf
Jackie Chen <cjc337993932@gmail.com>
2024-04-02 23:28:26 -0400

    Formatting

--- Lab5.js
+++ Lab5.js
@@ -9,7 +9,81 @@ const module = {
   description: "Learn NodeJS",
   course: "CS5610",
 };
+const todos = [
+  {id: 1, title: "Task 1", completed: false},
+  {id: 2, title: "Task 2", completed: true},
+  {id: 3, title: "Task 3", completed: false},
+  {id: 4, title: "Task 4", completed: true},
+];
+
 const Lab5 = (app) => {
+  app.get("/a5/todos", (req, res) => {
+    const {completed} = req.query;
+    if (completed !== undefined) {
+      const completedBool = completed === "true";
+      const completedTodos = todos.filter(
+          (t) => t.completed === completedBool);
+      res.json(completedTodos);
+      return;
+    }
+    res.json(todos);
+  });
+  app.post("/a5/todos", (req, res) => {
+    const newTodo = {
+      ...req.body,
+      id: new Date().getTime(),
+    };
+    todos.push(newTodo);
+    res.json(newTodo);
+  });
+  app.get("/a5/todos/:id", (req, res) => {
+    const {id} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    res.json(todo);
+  });
+  app.delete("/a5/todos/:id", (req, res) => {
+    const {id} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.status(404)
+      .json({message: `Unable to delete Todo with ID ${id}`});
+      return;
+    }
+    todos.splice(todos.indexOf(todo), 1);
+    res.sendStatus(200);
+  });
+  app.put("/a5/todos/:id", (req, res) => {
+    const {id} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.status(404)
+      .json({message: `Unable to update Todo with ID ${id}`});
+      return;
+    }
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    todo.due = req.body.due;
+    todo.completed = req.body.completed;
+    res.sendStatus(200);
+  });
+  app.get("/a5/todos/:id/title/:title", (req, res) => {
+    const {id, title} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = title;
+    res.json(todos);
+  });
+  app.get("/a5/todos/:id/description/:newDescription", (req, res) => {
+    const {id, newDescription} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.description = newDescription;
+    res.json(todos);
+  });
+  app.get("/a5/todos/:id/completed/:newStatus", (req, res) => {
+    const {id, newStatus} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.completed = newStatus === "true";
+    res.json(todos);
+  });
   app.get("/a5/welcome", (req, res) => {
     res.send("Welcome to Assignment 5");
   });
@@ -67,6 +141,29 @@ const Lab5 = (app) => {
   });
   app.get("/a5/module", (req, res) => {
     res.json(module);
+  });
+  app.get("/a5/module/name", (req, res) => {
+    res.json(module.name);
+  });
+  app.get("/a5/module/name/:newName", (req, res) => {
+    const {newName} = req.params;
+    module.name = newName;
+    res.json(module);
+  });
+  app.get("/a5/module/description/:newDescription", (req, res) => {
+    const {newDescription} = req.params;
+    module.description = newDescription;
+    res.json(module);
+  });
+  app.get("/a5/assignment/score/:newScore", (req, res) => {
+    const {newScore} = req.params;
+    assignment.score = parseInt(newScore);
+    res.json(assignment);
+  });
+  app.get("/a5/assignment/completed/:newStatus", (req, res) => {
+    const {newStatus} = req.params;
+    assignment.completed = newStatus === "true";
+    res.json(assignment);
   });
 };
 export default Lab5;
