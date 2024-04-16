020bc6d410e9b99488ab0c4a734d94d5a75a236d
Jackie Chen <cjc337993932@gmail.com>
2024-04-15 22:17:44 -0400

    Finished A6

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import express from 'express';
+import express from "express";
 import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentsRoutes from "./Kanbas/assignments/routes.js";
+
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentsRoutes(app);
