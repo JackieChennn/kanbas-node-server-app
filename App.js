7fe047a3aa62cd8da23566cc74043d5282682b23
Jackie Chen <cjc337993932@gmail.com>
2024-04-15 23:19:59 -0400

    Formatting

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentsRoutes from "./Kanbas/assignments/routes.js";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors());
 app.use(express.json());
